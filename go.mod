6f8349d4044a26e5c8d546942f62fcdf2198fc78
chaoyuepan <chaoyuepan@baidu.com>
2021-08-17 13:34:00 +0800

    v1.0.0

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/panicthis/P
+
+go 1.17
+
+require github.com/panicthis/T v0.0.0-20210817053212-4dff466ae56d
---
+++ lib.go
@@ -0,0 +1,12 @@
+package P
+
+import (
+	"fmt"
+
+	"github.com/panicthis/T"
+)
+
+func Visit() {
+	fmt.Println("T 1.0.0")
+	T.Visit()
+}
